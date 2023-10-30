d1b13b65942ae2d144305c166185d67b49f2581b
peter202597 <peter202597@gmail.com>
2023-10-30 21:27:13 +0900

    FEAT : MenuContext 키 수정

--- src/MenuContext.java
+++ src/MenuContext.java
@@ -22,19 +22,30 @@ public class MenuContext {
 
     private void initializeMenuItens() {
         List<Menu> mainMenus = new ArrayList<>();
+
+        List<Product> coffeMenus = new ArrayList<>();
+        List<Product> beverageMenus = new ArrayList<>();
+        List<Product> icecreamDessertMenus = new ArrayList<>();
+        List<Product> paikSccinoMenus = new ArrayList<>();
+
         mainMenus.add(new Menu("커피", "뉴 크롭 원두를 사용하여 더욱더 신선한 커피 메뉴를 만나보세요."));
         mainMenus.add(new Menu("음료", "퀄리티 높은 식자재를 사용하여 맛있는 음료를 제공합니다."));
         mainMenus.add(new Menu("아이스크림/디저트", "달콤 아이스크림부터 든든 브레드까지 다양하게 즐기세요."));
         mainMenus.add(new Menu("빽스치노", "오직 빽다방에서만 만나보실 수 있는 시그니처 메뉴입니다."));
 
+        menuProduct.put("커피", coffeMenus);
+        menuProduct.put("음료", beverageMenus);
+        menuProduct.put("아이스크림/디저트", icecreamDessertMenus);
+        menuProduct.put("빽스치노", paikSccinoMenus);
+
         List<Menu> orderMenus = new ArrayList<>();
         orderMenus.add(new Menu("카트", "장바구니를 확인 후 주문합니다."));
         orderMenus.add(new Menu("취소", "진행중인 주문을 취소합니다."));
+        //Ctrl + Shift + 방향키 하면 애들 위아래로 움직임 개신기함
 
         menus.put("카테고리", mainMenus);
         menus.put("주문", orderMenus);
         //커피
-        List<Product> coffeMenus = new ArrayList<>();
         coffeMenus.add(new Product("더블에스프레소", 1.5, "브라질 원두 블렌딩으로 다크초콜릿, 카라멜, 견과류의 고소함을 느낄 수 있는 에스프레소"));
         coffeMenus.add(new Product("앗!메리카노", 1.5, "빽다방만의 맛과 향을 더한 100% 아라비카 로스팅 원두로 뽑아내 깊고 진한 맛의 아메리카노"));
         coffeMenus.add(new Product("원조커피", 2.5, "달콤한 빽다방 대표메뉴, 믹스커피 종결자!"));
@@ -47,7 +58,6 @@ public class MenuContext {
         coffeMenus.add(new Product("콜드브루라떼", 4.5, "콜드브루에 신선한 우유를 더한 콜드브루라떼"));
         coffeMenus.add(new Product("아이스티샷추가(아샷추)", 3.0, "별미중의 별미! 달콤한 복숭아 맛 아이스티에 에스프레소 샷 추가~"));
         //음료
-        List<Product> beverageMenus = new ArrayList<>();
         beverageMenus.add(new Product("크림소다", 4.5, "소다맛 에이드에 아이스크림과 토핑이 더해진 부드러운 크림소다"));
         beverageMenus.add(new Product("키르시 블렌딩 파인", 3.5, "체리베이스가 블렌딩되어 상큼하게 즐기는 파인애플맛 슬러시"));
         beverageMenus.add(new Product("키르시 블렌딩 콜라", 3.5, "에스프레소 샷과 체리베이스가 블렌딩된 색다른 맛의 콜라맛 슬러시"));
@@ -60,7 +70,6 @@ public class MenuContext {
         beverageMenus.add(new Product("밀크티", 3.5, "얼그레이의 향긋함을 더한 부드러운 밀크티"));
         beverageMenus.add(new Product("달콤아이스티", 2.5, "시원하게 즐기는 복숭아 맛 아이스티"));
         //아이스크림&디저트
-        List<Product> icecreamDessertMenus = new ArrayList<>();
         icecreamDessertMenus.add(new Product("크리미슈", 2.0, "비스킷과 부드러운 크림의 환상 조합!"));
         icecreamDessertMenus.add(new Product("크리미단팥빵", 2.0, "달콤한 팥과 크림이 듬뿍~"));
         icecreamDessertMenus.add(new Product("사라다빵", 3.5, "어린시절 빵집에서 사먹던 추억의 감자 사라다빵"));
@@ -73,7 +82,6 @@ public class MenuContext {
         icecreamDessertMenus.add(new Product("노말한소프트", 2.0, "입안에서 부드럽게 사르륵 녹는 마성의 아이스크림"));
         icecreamDessertMenus.add(new Product("달고나크런치", 2.5, "바삭한 달고나와 부드러운 아이스크림이 잘 어울리는 달고나크런치"));
         //빽스치노
-        List<Product> paikSccinoMenus = new ArrayList<>();
         paikSccinoMenus.add(new Product("원조빽스치노", 3.5, "원조커피의 시원한 변신~! 매력만점 커피 빽스치노"));
         paikSccinoMenus.add(new Product("딸기바나나빽스치노", 3.8, "상큼한 딸기와 달달한 바나나의 완전 시원한 만남!"));
         paikSccinoMenus.add(new Product("초코바나나빽스치노", 3.8, "달콤한 초코와 달달한 바나나의 완전 시원한 만남!"));
@@ -82,16 +90,16 @@ public class MenuContext {
         paikSccinoMenus.add(new Product("민트초코빽스치노", 3.8, "초코칩으로 더한 깊은 초콜릿의 맛과 민트의 상쾌함이 어우러진 진한 민트초코 빽스치노"));
         paikSccinoMenus.add(new Product("쿠키크런치빽스치노", 3.8, "달콤한 쿠키 크런치가 듬뿍 들어간 빽스치노"));
         paikSccinoMenus.add(new Product("퐁당치노", 3.8, "빽다방 대표 시그니처 메뉴에 아낌없이 퐁당!"));
-
-        menuProduct.put("Coffe", coffeMenus);
-        menuProduct.put("Beverage", beverageMenus);
-        menuProduct.put("IcecreamDessert", icecreamDessertMenus);
-        menuProduct.put("PaikSccino", paikSccinoMenus);
     }
     public List<Menu> getMenus(String key) {
         return menus.get(key);
     }
     public List<Product> getMenuProducts(String key) {
+        List<Product> products = menuProduct.get(key);
+        // 오류부분 찾을 때 쓸 수 있음
+//        if (products == null) {
+//            throw new RuntimeException("키값오류");
+//        }
         return menuProduct.get(key);
     }
     public void addToCart(Product menuProduct) {
